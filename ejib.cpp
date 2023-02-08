9469324e100f9b0723e0c978448e41ac22c8ab19
Jibel <itsjibel@gmail.com>
2023-02-08 14:06:12 +0330

    First CLI commit

---
+++ ejib.cpp
@@ -0,0 +1,46 @@
+#include "Headers/mingw.thread.h"
+#include "Headers/user_input.h"
+using std::thread;
+class Jibel: public EditCommand {
+    private:
+        bool _enterToCommantMode=false;
+
+    public:
+        void runSystem() {
+            while (1) {
+                if (mode == "edit") {
+                    EDIT_SYSTEM();
+                    _enterToCommantMode= true;
+                }
+                
+                if (mode == "command") {
+                    if (_enterToCommantMode)
+                        system ("cls");
+                    _enterToCommantMode=false;
+                    commandLine();
+                }
+
+                if (mode == "visual") {
+                    ShowConsoleCursor(false);
+                    gotoxy (0, TerminalLine - 2);
+                    string modeView;
+
+                    for (int i=0; i<TerminalColumn/2-6; i++) modeView+=" ";
+                    modeView += "-- VISUAL --";
+                    for (int i=0; i<TerminalColumn/2-6; i++) modeView+=" ";
+
+                    SetConsoleTextAttribute (hConsole, 97);
+                    cout<<modeView;
+                    ShowConsoleCursor(true);
+                    gotoxy (10, TerminalLine - 1);
+                    editLine();
+                }
+            }
+        }
+};
+int main() {
+	Jibel Editor;
+    thread TerminalColumnTheread = thread(&Editor::reSizeTerminal, Editor);
+	Editor.runSystem();
+    TerminalColumnTheread.join();
+}
